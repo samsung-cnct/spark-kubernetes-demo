e19721e7847c7ac9208eb4d8ba42285c158fc423
Kwangsun Noh <nohkwangsun@gmail.com>
2016-02-10 16:04:43 -0800

    update commands

--- brightics-on-kubernetes/start-spark.sh
+++ brightics-on-kubernetes/start-spark.sh
@@ -1,26 +1,26 @@
 status=0
 
 printf "Start spark-master...\n"
-/Users/pieuler/kubernetes/cluster/kubectl.sh create -f spark-master-service.yaml
-/Users/pieuler/kubernetes/cluster/kubectl.sh create -f spark-master-controller.yaml
+kubectl.sh create -f spark-master-service.yaml
+kubectl.sh create -f spark-master-controller.yaml
 
-/Users/pieuler/kubernetes/cluster/kubectl.sh get pods
-while [ $(/Users/pieuler/kubernetes/cluster/kubectl.sh get pods | grep spark-master | grep Running | wc -l) -le 0 ]
+kubectl.sh get pods
+while [ $(kubectl.sh get pods | grep spark-master | grep Running | wc -l) -le 0 ]
 do
-  /Users/pieuler/kubernetes/cluster/kubectl.sh get pods
+  kubectl.sh get pods
   sleep 1
 done
 
 sleep 3
 
 printf "\nStart spark-worker...\n"
-/Users/pieuler/kubernetes/cluster/kubectl.sh create -f spark-worker-controller.yaml
-/Users/pieuler/kubernetes/cluster/kubectl.sh get pods
-while [ $(/Users/pieuler/kubernetes/cluster/kubectl.sh get pods | grep spark-worker | grep Pending | wc -l) -gt 0 ]
+kubectl.sh create -f spark-worker-controller.yaml
+kubectl.sh get pods
+while [ $(kubectl.sh get pods | grep spark-worker | grep Pending | wc -l) -gt 0 ]
 do
-  /Users/pieuler/kubernetes/cluster/kubectl.sh get pods
+  kubectl.sh get pods
   sleep 1
 done
 
-/Users/pieuler/kubernetes/cluster/kubectl.sh get pods
+kubectl.sh get pods
 printf "\nDone!!\n"
--- brightics-on-kubernetes/stop-spark.sh
+++ brightics-on-kubernetes/stop-spark.sh
@@ -1,16 +1,16 @@
 status=0
 
 printf "Stop spark...\n"
-/Users/pieuler/kubernetes/cluster/kubectl.sh delete -f spark-master-controller.yaml 
-/Users/pieuler/kubernetes/cluster/kubectl.sh delete -f spark-worker-controller.yaml 
-/Users/pieuler/kubernetes/cluster/kubectl.sh delete -f spark-master-service.yaml 
+kubectl.sh delete -f spark-master-controller.yaml 
+kubectl.sh delete -f spark-worker-controller.yaml 
+kubectl.sh delete -f spark-master-service.yaml 
 
-/Users/pieuler/kubernetes/cluster/kubectl.sh get pods
-while [ $(/Users/pieuler/kubernetes/cluster/kubectl.sh get pods | grep -E 'spark-master|spark-worker' | grep Terminating | wc -l) -gt 0 ]
+kubectl.sh get pods
+while [ $(kubectl.sh get pods | grep -E 'spark-master|spark-worker' | grep Terminating | wc -l) -gt 0 ]
 do
-  /Users/pieuler/kubernetes/cluster/kubectl.sh get pods
+  kubectl.sh get pods
   sleep 1
 done
 
-/Users/pieuler/kubernetes/cluster/kubectl.sh get pods
+kubectl.sh get pods
 printf "\nDone!!\n"
